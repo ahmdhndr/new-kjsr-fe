6336d3ec222f97dde3f932e03d4bec7cb752d3c4
Achmad Hendarsyah <ahmdhndrsyh@gmail.com>
2025-09-28 23:26:33 +0700

    fix: article not show on landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,24 @@
+import { buildURLParams } from "@/lib/build-params";
+import { QueryPaginationInterface } from "@/shared/interfaces/pagination.interface";
 import { articleServices } from "@/shared/services/article.service";
 
 import HomeComponent from "./home-component";
 
-export default async function Home() {
-  const { data: articles } = await articleServices.listArticle("");
+type SearchParams = Promise<QueryPaginationInterface>;
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const { page, search, category } = await searchParams;
+  const paginateQuery = buildURLParams({
+    page: page ?? 1,
+    limit: 10,
+    search: search ?? "",
+    category: category ?? "",
+  });
+  const { data: articles } = await articleServices.listArticle(paginateQuery);
 
   return (
     <>
